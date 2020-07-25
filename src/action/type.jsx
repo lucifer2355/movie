f5ce70b1103fd4e2c045c059dea765e668ec5809
Dhruvil Gajjar <dgajjar999@gmail.com>
2020-07-25 21:21:20 +0530

    add movie api

---
+++ src/action/type.jsx
@@ -0,0 +1,2 @@
+export default GET_CONFIG = "GET_CONFIG";
+export default GET_GENRES = "GET_GENRES";
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className='app'>
       <h1>Hello World</h1>
+      <h1>{process.env.REACT_APP_API}</h1>
     </div>
   );
 }
